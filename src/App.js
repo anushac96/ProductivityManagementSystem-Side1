678641b2e7f0528bc38c248035ae57169bd09138
anushac96 <anushac96>
2024-01-06 15:22:07 +0530

    added basic structure of calendar

--- src/App.js
+++ src/App.js
@@ -3,25 +3,42 @@ import Timer from './Timer';
 import Settings from './Settings';
 import { useState } from 'react';
 import SettingsContext from './SettingsContext';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 function App() {
 
   const [showSettings, setShowSettings] = useState(false);
-  const [workMinutes, setWorkMinutes] = useState(45);
-  const [breakMinutes, setBreakMinutes] = useState(15)
+  const [workMinutes, setWorkMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(5)
   return (
-    <main>
+    <><main>
       <SettingsContext.Provider value={{
-        showSettings, 
+        showSettings,
         setShowSettings,
         workMinutes,
         breakMinutes,
         setWorkMinutes,
         setBreakMinutes,
       }}>
-        {showSettings ? <Settings/> : <Timer/>}
+        {showSettings ? <Settings /> : <Timer />}
       </SettingsContext.Provider>
     </main>
+
+    <calendar>
+    <div class="container">
+      <div class="left">
+        <div class="calendarTag">
+          <div class="month">
+            <i class="fas fa-angle-left prev"></i>
+            <div class="date">January 2024</div>
+            <i class="fas fa-angle-right next"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    </calendar></>
   );
 }
 
