f5db08cba8a6fcf4b93cb0f0c597fac1b2cd2c8a
anushac96 <anushac96@gmail.com>
2024-03-04 19:53:53 -0800

    created db

---
+++ src/Query.sql
@@ -0,0 +1,15 @@
+CREATE TABLE KaryaDB.users (
+    userId INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(24) UNIQUE NOT NULL,
+    userpwd VARCHAR(255) NOT NULL,
+    useremailid VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE KaryaDB.events (
+    eventId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    title VARCHAR(255) NOT NULL,
+    startTime DATETIME NOT NULL,
+    endTime DATETIME NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users(userId)
+);
